9fc8aac21d5eaa18f5e51d810d353c61accf41dc
Douglas17471 <157817566+Douglas17471@users.noreply.github.com>
2024-03-18 11:53:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ const page1 = document.querySelector(".page1")
 const page2 = document.querySelector(".page2")
 const imgBtn = document.querySelector("#imgBtn")
 const btnReset = document.querySelector("#btnReset")
+const fortune = [
+  "O aprendizado é como o horizonte: não há limites.",
+  "Não há que ser forte, há que ser flexível.",
+  "Limitações são fronteiras criadas apenas pela nossa mente.",
+  "O cão não ladra por valentia e sim por medo.",
+  "Procure acender uma vela em vez de amaldiçoar a escuridão.",
+  "A palavra é prata, o silêncio é ouro.",
+  "Lembre-se de que grandes realizações e grandes amores envolvem grandes riscos.",
+  "Um pouco de perfume sempre fica nas mãos de quem oferece flores.",
+  "O homem só envelhece quando os lamentos substituem seus sonhos.",
+  "A persistência realiza o impossível.",
+  "Se alguém está tão cansado que não possa te dar um sorriso, deixa-lhe o teu.",
+]
+
 
 // Eventos
 page1.addEventListener('click', advanceClick)
@@ -11,8 +25,10 @@ document.addEventListener('keydown', ent)
 
 
 // Funções callback
+
 function advanceClick() {
   togglePage()
+  luck()
   // page1.classList.add("hide")
   // page2.classList.remove("hide")
 }
@@ -34,3 +50,12 @@ function ent(e) {
     advanceClick()
   }
 }
+
+function luck() {
+  let allFortunes = fortune.length
+  let randomNumber = Math.floor(Math.random() * allFortunes)  
+  page2.querySelector("p").innerText = `${fortune[randomNumber]}`
+}
+
+
+
